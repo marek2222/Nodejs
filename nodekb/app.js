a5fb07e35d028cf6d5ef063e10dd7301e4891f75
marek2222 <marekmusial2@o2.pl>
2018-09-17 15:29:43 +0200

    Express: 03_3.2:   install nodemon. nodemon reload server, automatically after each change. Install language-pug

--- nodekb/app.js
+++ nodekb/app.js
@@ -11,7 +11,37 @@ app.set('view engine', 'pug');
 
 //Home route
 app.get('/', function(req, res){
-  res.render('index');
+  let articles = [
+    {
+      id: 1,
+      title: 'Article One',
+      author: 'Brad Traversy',
+      body: 'This is article one'
+    },
+    {
+      id: 2,
+      title: 'Article Two',
+      author: 'John Doe',
+      body: 'This is article two'
+    },
+    {
+      id: 3,
+      title: 'Article Three',
+      author: 'Brad Traversy',
+      body: 'This is article three'
+    },
+  ];
+  res.render('index', {
+    title: 'Articles',
+    articles: articles
+  });
+});
+
+// Add route
+app.get('/articles/add', function(req, res){
+  res.render('add_article', {
+    title: 'Add Article'
+  });
 });
 
 /// Start server
