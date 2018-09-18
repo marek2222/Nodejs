5fb6d64c4fe898223db63024caa72ce7903c41b6
marek2222 <marekmusial2@o2.pl>
2018-09-18 14:18:00 +0200

    Express: 03_6:  Install bower and add detail of article
    > npm install -g bower
    To install latest version npm:
    > npm install npm@latest -g
    
    Create public directory: public/bower_components
    > bower install bootstrap
    > bower install jQuery

--- nodekb/app22.js
+++ nodekb/app22.js
@@ -11,12 +11,12 @@ db.once('open', function(){
   console.log('Connected to MongoDB');
 });
 
-// Check for Db errors
+// Check for DB errors
 db.on('error', function(err){
   console.log(err);
 });
 
-// Init app
+// Init App
 const app = express();
 
 // Bring in Models
@@ -26,21 +26,16 @@ let Article = require('./models/article');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// Body Parser Middleware - To handle HTTP POST request in Express.js version 4 and above, you need to install middleware module called body-parser. body-parser extract the entire body portion of an incoming request stream and exposes it on req.body.
+// Body Parser Middleware
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-// create application/json parser
-var jsonParser = bodyParser.json();
-// create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
-
-// Set Public folder: to images, Css, files
+// Set Public Folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-//Home route
+// Home Route
 app.get('/', function(req, res){
   Article.find({}, function(err, articles){
     if(err){
@@ -54,15 +49,14 @@ app.get('/', function(req, res){
   });
 });
 
-// Get single Articles
+// Get Single Article
 app.get('/article/:id', function(req, res){
   Article.findById(req.params.id, function(err, article){
     res.render('article', {
-      article: article
+      article:article
     });
   });
 });
-
 
 // Add Route
 app.get('/articles/add', function(req, res){
@@ -77,6 +71,7 @@ app.post('/articles/add', function(req, res){
   article.title = req.body.title;
   article.author = req.body.author;
   article.body = req.body.body;
+
   article.save(function(err){
     if(err){
       console.log(err);
@@ -87,12 +82,46 @@ app.post('/articles/add', function(req, res){
   });
 });
 
+// Load Edit Form
+app.get('/article/edit/:id', function(req, res){
+  Article.findById(req.params.id, function(err, article){
+    res.render('edit_article', {
+      title:'Edit Article',
+      article:article
+    });
+  });
+});
 
+// Update Submit POST Route
+app.post('/articles/edit/:id', function(req, res){
+  let article = {};
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body = req.body.body;
 
+  let query = {_id:req.params.id}
 
+  Article.update(query, article, function(err){
+    if(err){
+      console.log(err);
+      return;
+    } else {
+      res.redirect('/');
+    }
+  });
+});
 
+// Delete Article
+app.delete('/article/:id', function(req, res){
+  let query = {_id:req.params.id}
 
-
+  Article.remove(query, function(err){
+    if(err){
+      console.log(err);
+    }
+    res.send('Success');
+  });
+});
 
 // Start Server
 app.listen(3000, function(){
